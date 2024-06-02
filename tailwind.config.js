9712b5eebadb72131fa02c3d6e3427cc0350f7f4
EdiedRamos <edieisa22@gmail.com>
2024-06-02 18:35:46 -0500

    chore: update tailwind.config and add assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "cc-white": "#FFFFFE",
+        "cc-black": "#121826",
+        "cc-dark": "#364153",
+        "cc-light": "#CED6E1",
+        "cc-blue": "#406AFF",
+      },
+      fontSize: {
+        "cs-50": "10px",
+        "cs-100": "16px",
+        "cs-200": "32px",
+        "cs-300": "40px",
+      },
+    },
   },
   plugins: [],
 };
