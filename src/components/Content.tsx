405ea24f6593abe9bc8ca7c1dd6cbeb2657c3648
EdiedRamos <edieisa22@gmail.com>
2024-06-03 05:01:20 -0500

    style: adjust style

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -5,10 +5,10 @@ export const Content = () => {
   return (
     <div className="flex flex-col items-center">
       <NoteCodeLogo />
-      <p className="mt-3 md:mt-8 text-cs-200 font-medium text-cc-dark">
+      <p className="mt-3 md:mt-8 text-cs-200 font-medium text-cc-black">
         Create & Share
       </p>
-      <p className="mt-1 md:mt-2 text-cs-300 font-medium text-cc-dark">
+      <p className="mt-1 text-cs-300 font-medium text-cc-black">
         Your Code easily
       </p>
       <div className="mt-3 md:mt-8 mb-20">
