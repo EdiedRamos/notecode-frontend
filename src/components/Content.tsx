008346887b64e5b9870e805a52279b810ac55573
EdiedRamos <edieisa22@gmail.com>
2024-06-03 04:57:03 -0500

    style: adjust style

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -6,8 +6,8 @@ export const Content = () => {
     <div className="flex flex-col items-center">
       <NoteCodeLogo />
       <p className="mt-3 md:mt-8 text-cs-200 font-bold">Create & Share</p>
-      <p className="mt-1 md:mt-3 text-cs-300 font-bold">Your Code easily</p>
-      <div className="mt-3 md:mt-9 mb-20">
+      <p className="mt-1 md:mt-2 text-cs-300 font-bold">Your Code easily</p>
+      <div className="mt-3 md:mt-8 mb-20">
         <MonacoEditor />
       </div>
     </div>
