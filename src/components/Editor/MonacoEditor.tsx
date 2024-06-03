770f1df0b62cdbbb155c6c46f12571093519d2cc
EdiedRamos <edieisa22@gmail.com>
2024-06-03 01:22:26 -0500

    feat: implement core logic

--- src/components/Editor/MonacoEditor.tsx
+++ src/components/Editor/MonacoEditor.tsx
@@ -2,7 +2,7 @@ import { Editor } from "@monaco-editor/react";
 import { EditorLanguages } from "./EditorLanguages";
 import { EditorTheme } from "./EditorTheme";
 import { Language, type Theme } from "@/types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { LinkIcon, ShareIcon } from "@/assets";
 
 const DEVAULT_CONTENT = `<html>
@@ -27,6 +27,9 @@ const DEVAULT_CONTENT = `<html>
 export const MonacoEditor = () => {
   const [theme, setTheme] = useState<Theme>("vs-dark");
   const [language, setLanguage] = useState<Language>("html");
+  const [codeId, setCodeId] = useState<string>("");
+  const [code, setCode] = useState<string>(DEVAULT_CONTENT);
+  const [shareDisable, setShareDisabled] = useState<boolean>(false);
 
   const handleTheme = (newTheme: Theme) => {
     setTheme(newTheme);
@@ -35,6 +38,31 @@ export const MonacoEditor = () => {
   const handleLanguage = (newLanguage: Language) => {
     setLanguage(newLanguage);
   };
+
+  const handleShare = () => {
+    const uuid = crypto.randomUUID();
+    history.pushState({}, "", `/${uuid}`);
+    setCodeId(uuid);
+    setShareDisabled(true);
+    // TODO MAKE POST REQUEST
+    localStorage.setItem("code", code);
+  };
+
+  const handleChange = (code: string | undefined) => {
+    if (!code) return;
+    setCode(code);
+    setShareDisabled(false);
+  };
+
+  useEffect(() => {
+    const uuid = location.pathname.substring(1);
+    if (uuid.length === 0) return;
+    setCodeId(uuid);
+    setShareDisabled(true);
+    // TODO MAKE GET REQUEST
+    const codeSaved = localStorage.getItem("code");
+    codeSaved && setCode(codeSaved);
+  }, []);
 
   return (
     <div
@@ -46,8 +74,9 @@ export const MonacoEditor = () => {
         theme={theme}
         height={"720px"}
         width={"100%"}
+        value={code}
         language={language}
-        defaultValue={DEVAULT_CONTENT}
+        onChange={handleChange}
       />
       <div className="mt-3 flex flex-wrap gap-3 justify-between">
         <div className="flex flex-wrap gap-3 w-full md:w-auto justify-center">
@@ -55,11 +84,17 @@ export const MonacoEditor = () => {
           <EditorTheme handleTheme={handleTheme} />
         </div>
         <div className="flex flex-wrap gap-5 w-full md:w-auto justify-center">
-          <button className="flex items-center gap-2">
-            <LinkIcon />
-            <p className="text-gray-500">.../2NZNOWELPo</p>
-          </button>
-          <button className="p-2 flex flex-wrap items-center gap-3 bg-blue-500 text-white rounded-xl disabled:bg-gray-500">
+          {codeId.length > 0 && (
+            <button className="flex items-center gap-2">
+              <LinkIcon />
+              <p className="text-gray-500">.../{codeId.substring(0, 10)}</p>
+            </button>
+          )}
+          <button
+            disabled={shareDisable}
+            className="p-2 flex flex-wrap items-center gap-3 bg-blue-500 text-white rounded-xl disabled:bg-gray-500"
+            onClick={handleShare}
+          >
             <ShareIcon />
             Share
           </button>
