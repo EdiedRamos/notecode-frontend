9e3e8c12d20e7c20e8e499ba2a0d7f26b90658ff
EdiedRamos <edieisa22@gmail.com>
2024-06-03 05:09:59 -0500

    style: adjust style

--- src/layouts/MainBackground.tsx
+++ src/layouts/MainBackground.tsx
@@ -7,7 +7,7 @@ interface Props {
 export const MainBackground = ({ children }: Props) => {
   return (
     <div className="relative bg-purple-500 min-h-[100vh]">
-      <div className="select-none h-[600px]">
+      <div className="select-none h-[700px]">
         <img
           className="bg-purple-500 object-cover object-bottom w-full h-full"
           src={HeroBackground}
